f5a4d8ef2ef1eb0672bd32d329a2b2dbe6bd0c6a
felldek <felldek1@gmail.com>
2020-09-18 17:47:24 +0300

    init commit

---
+++ index.js
@@ -0,0 +1,27 @@
+const express = require("express");
+const routes = require("./routes/routes");
+const PORT = process.env.PORT || 3004;
+const host = `https://server-to-do-list.herokuapp.com/`;
+const hostToDoList = "https://todolist-3y09gy1o1.vercel.app/"
+const app = express();
+
+app.listen(PORT, () => {
+  console.log("Server has been started...");
+});
+
+app.use(express.urlencoded());
+app.use(express.json());
+app.use((req, res, next) => {
+  res.append("Access-Control-Allow-Origin", ["*"]);
+  res.append("Access-Control-Allow-Methods", "GET,PUT,POST,DELETE");
+  res.append("Access-Control-Allow-Headers", "Content-Type");
+  next();
+});
+
+
+
+app.use("/", routes);
+
+module.exports.PORT = PORT;
+module.exports.host = host;
+module.exports.hostToDoList = hostToDoList;
